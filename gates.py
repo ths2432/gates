53f20559e6a84b958c670642a23a487b0628bb17
ths2432 <ths2432@towerhill.org>
2023-05-05 12:51:25 -0400

    Refactor + some broken code

--- gates.py
+++ gates.py
@@ -1,52 +1,69 @@
-class Gate:
-    def __init__(self, func):
-        self.func = func
-        self.inputs = []
+# representation of https://www.allaboutcircuits.com/textbook/digital/chpt-7/circuit-simplification-examples/
 
-    def eval(self):
-        inputs = []
-        for input in self.inputs:
-            inputs.append(input.eval())
-        return self.func(inputs)
+# TODO: should operations be made binary?
+
+circuit = {
+    "op": "+", "operands": [
+        { "op": "*", "operands": [ "a", "b" ] },
+        {
+            "op": "*", "operands": [
+                "b",
+                "c",
+                { "op": "+", "operands": [ "b", "c" ] }
+            ]
+        }
+    ]
+}
+
+ops = {
+    "+": lambda a, b: a or b,
+    "*": lambda a, b: a and b
+}
+
+def eval_circuit(circuit, inputs):
+    if isinstance(circuit, str):
+        return inputs[circuit]
+
+    operands = []
+    for operand in circuit["operands"]:
+        operands.append(eval_circuit(operand, inputs))
+
+    result = operands[0]
+    for i in range(1, len(operands)):
+        result = ops[circuit["op"]](result, operands[i])
+    return result
 
 
-def buffer_gate(inputs):
-    if len(inputs) != 1:
-        raise ValueError
+def expression(circuit):
+    if isinstance(circuit, str):
+        return circuit
 
-    return inputs[0]
-
-
-def not_gate(inputs):
-    if len(inputs) != 1:
-        raise ValueError
-
-    return not inputs[0]
+    result = "(" + expression(circuit["operands"][0])
+    for i in range(1, len(circuit["operands"])):
+        result += " " + circuit["op"] + " " + expression(circuit["operands"][i])
+    result += ")"
+    return result
 
 
-def and_gate(inputs):
-    output = True
-    for input in inputs:
-        output = output and input
-    return output
+def truth_table(circuit, inputs):
+    inputs = {(i, False) for i in inputs}
+    for i in range(2 ** len(inputs)):
+        for j in range(len(inputs)):
+            if inputs[len(inputs) - 1 - j] == True:
+                inputs[len(inputs) - 1 - j] = False
+            else:
+                inputs[len(inputs) - 1 - j] = True
+                break
+
+        print()
+        print(inputs)
+        print(eval_circuit(circuit, inputs))
 
 
-def or_gate(inputs):
-    output = False
-    for input in inputs:
-        output = output or input
-    return output
-    
 
 def simplify(circuit):
     raise NotImplementedError
 
-
-root = Gate(and_gate)
-org = Gate(or_gate)
-a = Gate(lambda inputs: True)
-b = Gate(lambda inputs: True)
-c = Gate(lambda inputs: False)
-org.inputs = [ a, b ]
-root.inputs = [ org, c ]
-print(root.eval())
+print(eval_circuit(circuit, {"a": True, "b": False, "c": True}))
+print(expression(circuit))
+truth_table(circuit, [ "a", "b", "c" ])
