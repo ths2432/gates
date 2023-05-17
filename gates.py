08293a05f11b2d97a7d4d1980c97146bf8999458
ths2432 <ths2432@towerhill.org>
2023-05-17 11:34:53 -0400

    Add NotGate and switch to the more standard notation

--- gates.py
+++ gates.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import itertools
 
 class Node(ABC):
     def __init__(self, children):
@@ -8,31 +9,33 @@ class Node(ABC):
     def __str__(self):
         pass
 
+    def __repr__(self):
+        return str(self)
+
     @abstractmethod
-    def eval(self):
+    def eval(self, inputs):
         pass
 
-    def inputs(self):
-        inputs = set()
+    def input_nodes(self):
+        nodes = set()
         for child in self.children:
             if isinstance(child, Input):
-                inputs.add(child)
+                nodes.add(child)
             else:
-                inputs = inputs.union(child.inputs())
-        return inputs
+                nodes = nodes.union(child.input_nodes())
+        return nodes
 
 
 class Input(Node):
-    def __init__(self, name, value):
+    def __init__(self, name):
         super().__init__([])
         self.name = name
-        self.value = value
 
     def __str__(self):
         return self.name
 
-    def eval(self):
-        return self.value
+    def eval(self, inputs):
+        return inputs[self]
 
 
 class AndGate(Node):
@@ -40,12 +43,12 @@ class AndGate(Node):
         super().__init__(children)
 
     def __str__(self):
-        return f"{''.join([str(child) for child in self.children])}"
+        return f"({' ∧ '.join([str(child) for child in self.children])})"
 
-    def eval(self):
+    def eval(self, inputs):
         value = True
         for child in self.children:
-            value = value and child.eval()
+            value = value and child.eval(inputs)
         return value
 
 
@@ -54,20 +57,34 @@ class OrGate(Node):
         super().__init__(children)
 
     def __str__(self):
-        return f"({' + '.join([str(child) for child in self.children])})"
+        return f"({' ∨ '.join([str(child) for child in self.children])})"
 
-    def eval(self):
+    def eval(self, inputs):
         value = False
         for child in self.children:
-            value = value or child.eval()
+            value = value or child.eval(inputs)
         return value
+
+
+class NotGate(Node):
+    def __init__(self, children):
+        if len(children) != 1:
+            raise ValueError("NotGate must have exactly one child")
+
+        super().__init__(children)
+
+    def __str__(self):
+        return f"¬{str(self.children[0])}"
+
+    def eval(self, inputs):
+        return not self.children[0].eval(inputs)
 
 
 # terrible testing
 # ab + bc(b + c)
-a = Input("a", False)
-b = Input("b", False)
-c = Input("c", False)
+a = Input("a")
+b = Input("b")
+c = Input("c")
 ab = AndGate([ a, b ])
 o = OrGate([ b, c ])
 bc = AndGate([ b, c, o ])
