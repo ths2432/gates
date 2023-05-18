fb80f9391e178be0602dda731a783e3899c84487
ths2432 <ths2432@towerhill.org>
2023-05-18 11:08:25 -0400

    Refactoring

--- gates.py
+++ gates.py
@@ -1,22 +1,16 @@
 from abc import ABC, abstractmethod
-import copy
-import random
+from copy import deepcopy
+from pprint import pprint
 
-# TODO:
-# - Implement the not gate
-# - Add the rest of the rules
-# - Simulated annealing maybe?
-
-class Node(ABC):
+class Node:
     def __init__(self, children):
         self.children = children
-        pass
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):
             return False
 
-        return self.children == other.children
+        return other.children == self.children
 
     @abstractmethod
     def __str__(self):
@@ -25,141 +19,127 @@ class Node(ABC):
     def __repr__(self):
         return str(self)
 
-    def matches(self, expr, vars={}):
-        if type(expr) == Var:
-            if expr.name in vars:
-                if self != vars[expr.name]:
-                    return False
+    def match(self, form, variables):
+        if isinstance(form, Var):
+            if form.name in variables:
+                if self != variables[form.name]:
+                    return None
             else:
-                vars[expr.name] = self
-                return True
+                variables[form.name] = self
+                return variables
 
-        if not isinstance(self, type(expr)):
-            return False
+        if not isinstance(self, type(form)):
+            return None
 
-        if not len(self.children) == len(expr.children):
-            return False
-        
+        if len(self.children) != len(form.children):
+            return None
+
         for i in range(len(self.children)):
-            if not self.children[i].matches(expr.children[i], vars):
-                return False
+            if self.children[i].match(form.children[i], variables) is None:
+                return None
 
-        return True
+        return variables
 
-    def substitute(self, vars):
-        result = copy.deepcopy(self)
+    def substitute(self, variables):
+        result = deepcopy(self)
         for i in range(len(result.children)):
             if isinstance(result.children[i], Var):
-                result.children[i] = vars[result.children[i].name]
+                result.children[i] = variables[result.children[i].name]
             else:
-                result.children[i] = result.children[i].substitute(vars)
+                result.children[i] = result.children[i].substitute(variables)
         return result
 
-    def neighbors(self, rules):
+    def neighbors(self):
         neighbors = []
 
-        if hasattr(self, "children"):
-            for i in range(len(self.children)):
-                for neighbor in self.children[i].neighbors(rules):
-                    full = copy.deepcopy(self)
-                    full.children[i] = neighbor
-                    neighbors.append(full)
+        for rule in RULES:
+            for variant in rule.variants:
+                variables = self.match(variant.left, {})
+                if variables is not None:
+                    neighbor = variant.right.substitute(variables)
+                    if neighbor not in neighbors:
+                        neighbors.append(neighbor)
 
-        for rule in rules:
-            vars = {}
-            if self.matches(rule.left, vars):
-                neighbors.append(rule.right.substitute(vars))
-            
+        for i in range(len(self.children)):
+            for child_neighbor in self.children[i].neighbors():
+                neighbor = deepcopy(self)
+                neighbor.children[i] = child_neighbor
+                if neighbor not in neighbors:
+                    neighbors.append(neighbor)
+
         return neighbors
-
-    def complexity(self):
-        complexity = 1
-        if hasattr(self, "children"):
-            for child in self.children:
-                complexity += child.complexity()
-        return complexity
-
-    def simplify(self, rules):
-        state = self
-        while True:
-            print(state)
-            neighbors = self.neighbors(rules)
-            state = random.choice(neighbors)
-
 
 class Var(Node):
     def __init__(self, name):
+        super().__init__([])
         self.name = name
 
     def __eq__(self, other):
-        if not isinstance(other, Var):
-            return False
-
-        return self.name == other.name
+        return super().__eq__(other) and other.name == self.name
 
     def __str__(self):
         return self.name
 
-class Or(Node):
-    def __init__(self, left, right):
-        super().__init__([left, right])
+class Not(Node):
+    def __init__(self, child):
+        super().__init__([child])
 
     def __str__(self):
-        return f"({self.children[0]} + {self.children[1]})"
+        return f"!{self.children[0]}"
 
-class And(Node):
-    def __init__(self, left, right):
+class Binary(Node):
+    def __init__(self, symbol, left, right):
         super().__init__([left, right])
+        self.symbol = symbol
 
     def __str__(self):
-        return f"({self.children[0]} * {self.children[1]})"
+        return f"({f' {self.symbol} '.join([str(child) for child in self.children])})"
+
+class Or(Binary):
+    def __init__(self, left, right):
+        super().__init__("+", left, right)
+
+class And(Binary):
+    def __init__(self, left, right):
+        super().__init__("*", left, right)
 
 class Rule:
-    def __init__(self, left, right):
+    def __init__(self, left, right, inverse=None):
         self.left = left
         self.right = right
+        self.inverse = inverse or Rule(right, left, self)
+        self.variants = [ self, self.inverse ]
+
+    def __str__(self):
+        return f"{self.left} = {self.right}"
 
     def __repr__(self):
-        return str([self.left, self.right])
+        return str(self)
 
-rules = [
-    # Commutative laws
-    Rule(
-        Or(Var("a"), Var("b")),
-        Or(Var("b"), Var("a"))
-    ),
-    Rule(
-        And(Var("a"), Var("b")),
-        And(Var("b"), Var("a"))
-    ),
-    # Associative laws
-    Rule(
-        Or(Var("a"), Or(Var("b"), Var("c"))),
-        Or(Or(Var("a"), Var("b")), Var("c")),
-    ),
-    Rule(
-        And(Var("a"), And(Var("b"), Var("c"))),
-        And(And(Var("a"), Var("b")), Var("c")),
-    ),
-    # Distributive law
-    Rule(
-        And(Var("a"), Or(Var("b"), Var("c"))),
-        Or(And(Var("a"), Var("b")), And(Var("a"), Var("c"))),
-    ),
-    # Simplification theorems
-    Rule(
-        Or(Var("a"), Var("a")),
-        Var("a")
-    ),
-    Rule(
-        And(Var("a"), Var("a")),
-        Var("a")
-    ),
-    Rule(
-        Or(Var("a"), And(Var("a"), Var("b"))),
-        Var("a")
-    )
+RULES = [
+    # Commutative properties
+    # a + b = b + a
+    # a * b = b * a
+    Rule(Or(Var("a"), Var("b")), Or(Var("b"), Var("a"))),
+    Rule(And(Var("a"), Var("b")), And(Var("b"), Var("a"))),
+
+    # Associative properties
+    # a + (b + c) = (a + b) + c
+    # a * (b * c) = (a * b) * c
+    Rule(Or(Var("a"), Or(Var("b"), Var("c"))), Or(Or(Var("a"), Var("b")), Var("c"))),
+    Rule(And(Var("a"), And(Var("b"), Var("c"))), And(And(Var("a"), Var("b")), Var("c"))),
+
+    # Distributive property
+    # a * (b + c) = (a * b) + (a * c)
+    Rule(And(Var("a"), Or(Var("b"), Var("c"))), Or(And(Var("a"), Var("b")), And(Var("a"), Var("c")))),
+
+    # De Morgan's laws
+    # !(a + b) = !a * !b
+    # !(a * b) = !a + !b
+    Rule(Not(Or(Var("a"), Var("b"))), And(Not(Var("a")), Not(Var("b")))),
+    Rule(Not(And(Var("a"), Var("b"))), Or(Not(Var("a")), Not(Var("b"))))
 ]
 
 expr = Or(And(Var("a"), Var("b")), And(And(Var("b"), Var("c")), Or(Var("b"), Var("c"))))
-print(expr.simplify(rules))
+print(expr)
+pprint(expr.neighbors())
